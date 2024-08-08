c64b832b5f299e4fcf20299822f5780d79e9a288
Alex1000-3 <aleks.t1y.2.3@gmail.com>
2024-08-08 19:51:45 +0300

    scroll

--- legalservices/js/legalservices.js
+++ legalservices/js/legalservices.js
@@ -49,3 +49,39 @@ const fixedScorllBlock = () => {
    }
 }
 window.addEventListener('scroll', fixedScorllBlock)
+
+// анимация при скроле 
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+   window.addEventListener('scroll', animOnScroll);
+   function animOnScroll() {
+      for (let index = 0; index < animItems.length; index++) {
+         const animItem = animItems[index];
+         const animItemHeight = animItem.offsetHeight;
+         const animItemOffest = offset(animItem).top;
+         const animStart = 2;
+         let animItemPoint = window.innerHeight - animItemHeight / animStart;
+         if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+         }
+         if ((pageYOffset > animItemOffest - animItemPoint) && pageYOffset < (animItemOffest + animItemHeight)) {
+            animItem.classList.add('_active');
+         } else {
+            if (!animItem.classList.contains('_anim-no-hide')) {
+               animItem.classList.remove('_active');
+            }
+         }
+      }
+   }
+   function offset(el) {
+      const rect = el.getBoundingClientRect(), // ,
+         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+   }
+   // animOnScroll();
+   setTimeout(() =>{
+      animOnScroll();
+   }, 300);
+}
